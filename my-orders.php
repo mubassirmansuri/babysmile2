2ecf4f0b12667395580dd322f7226490afb4542b
mubassirmansuri <mubassirmansuri54@gmail.com>
2024-09-18 00:10:33 +0530

    changes

---
+++ my-orders.php
@@ -0,0 +1,290 @@
+<?php include 'include/header.php'?>
+
+<section class="breadcrumb-section">
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <div class="breadcrumb-item">
+          <h1 class="breadcrumb-title">My Orders</h1>
+          <ul class="breadcrumb-ul">
+            <li class="breadcrumb-li"> <a href="javascript:void(0)" class="breadcrumb-link">Home</a></li>
+            <li class="breadcrumb-li"> <i class="fi fi-rr-angle-right"></i> </li>
+            <li class="breadcrumb-li active">My Orders</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="order-track-section section-padding">
+  <div class="container">
+    <div class="row">
+      <div class="col-12 mb-60">
+        <h3 class="common-main-title mb-30">Current <span> Order</span></h3>
+        <div class="order-track-product-item">
+          <div class="order-track-product-cart">
+            <div class="order-track-product-mdiv">
+              <div class="order-track-product-left">
+                <a href="javascript:void(0)" class="order-track-img-div">
+                  <img src="./assets/images/product1.webp" alt="product 1">
+                </a>
+                <div class="order-track-detail-div">
+                  <a href="javascript:void(0)" class="order-track-name">100% Pure Cotton Jumpsuit</a>
+                  <p class="category">Quantity: <span class="fw-bold">4x</span></p>
+                  <p class="cart-price-p">₹100</p>
+                </div>
+              </div>
+              <div class="order-track-product-right">
+                <a href="javascript:void(0)" class="common-btn cancel-btn">Cancel Order</a>
+              </div>
+            </div>
+            <div class="order-track-product-mdiv">
+              <div class="order-track-product-left">
+                <a href="javascript:void(0)" class="order-track-img-div">
+                  <img src="./assets/images/product1.webp" alt="product 1">
+                </a>
+                <div class="order-track-detail-div">
+                  <a href="javascript:void(0)" class="order-track-name">100% Pure Cotton Jumpsuit</a>
+                  <p class="category">Quantity: <span class="fw-bold">4x</span></p>
+                  <p class="cart-price-p">₹100</p>
+                </div>
+              </div>
+              <div class="order-track-product-right">
+                <a href="javascript:void(0)" class="common-btn cancel-btn">Cancel Order</a>
+              </div>
+            </div>
+          </div>
+          <div class="order-track">
+            <div class="order-track-step step-done">
+              <div class="order-track-status">
+                <span class="order-track-status-dot"></span>
+              </div>
+              <div class="order-track-text">
+                <p class="order-track-text-stat">Order Received</p>
+                <span class="order-track-text-sub">21st November, 2024</span>
+              </div>
+            </div>
+            <div class="order-track-step step-done">
+              <div class="order-track-status">
+                <span class="order-track-status-dot"></span>
+              </div>
+              <div class="order-track-text">
+                <p class="order-track-text-stat">Order Processed</p>
+                <span class="order-track-text-sub">21st November, 2024</span>
+              </div>
+            </div>
+            <div class="order-track-step step-done">
+              <div class="order-track-status">
+                <span class="order-track-status-dot"></span>
+              </div>
+              <div class="order-track-text">
+                <p class="order-track-text-stat">Manufracturing In Progress</p>
+                <span class="order-track-text-sub">21st November, 2024</span>
+              </div>
+            </div>
+            <div class="order-track-step step-done">
+              <div class="order-track-status">
+                <span class="order-track-status-dot"></span>
+              </div>
+              <div class="order-track-text">
+                <p class="order-track-text-stat">Order Dispatched</p>
+                <span class="order-track-text-sub">21st November, 2024</span>
+              </div>
+            </div>
+            <div class="order-track-step step-done">
+              <div class="order-track-status">
+                <span class="order-track-status-dot"></span>
+              </div>
+              <div class="order-track-text">
+                <p class="order-track-text-stat">Order Deliverd</p>
+                <span class="order-track-text-sub">21st November, 2024</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="order-track-product-item">
+          <div class="order-track-product-cart">
+            <div class="order-track-product-mdiv">
+              <div class="order-track-product-left">
+                <a href="javascript:void(0)" class="order-track-img-div">
+                  <img src="./assets/images/product1.webp" alt="product 1">
+                </a>
+                <div class="order-track-detail-div">
+                  <a href="javascript:void(0)" class="order-track-name">100% Pure Cotton Jumpsuit</a>
+                  <p class="category">Quantity: <span class="fw-bold">4x</span></p>
+                  <p class="cart-price-p">₹100</p>
+                </div>
+              </div>
+              <div class="order-track-product-right">
+                <a href="javascript:void(0)" class="common-btn cancel-btn">Cancel Order</a>
+              </div>
+            </div>
+            <div class="order-track-product-mdiv">
+              <div class="order-track-product-left">
+                <a href="javascript:void(0)" class="order-track-img-div">
+                  <img src="./assets/images/product1.webp" alt="product 1">
+                </a>
+                <div class="order-track-detail-div">
+                  <a href="javascript:void(0)" class="order-track-name">100% Pure Cotton Jumpsuit</a>
+                  <p class="category">Quantity: <span class="fw-bold">4x</span></p>
+                  <p class="cart-price-p">₹100</p>
+                </div>
+              </div>
+              <div class="order-track-product-right">
+                <a href="javascript:void(0)" class="common-btn cancel-btn">Cancel Order</a>
+              </div>
+            </div>
+          </div>
+          <div class="order-track">
+            <div class="order-track-step step-done">
+              <div class="order-track-status">
+                <span class="order-track-status-dot"></span>
+              </div>
+              <div class="order-track-text">
+                <p class="order-track-text-stat">Order Received</p>
+                <span class="order-track-text-sub">21st November, 2024</span>
+              </div>
+            </div>
+            <div class="order-track-step step-done">
+              <div class="order-track-status">
+                <span class="order-track-status-dot"></span>
+              </div>
+              <div class="order-track-text">
+                <p class="order-track-text-stat">Order Processed</p>
+                <span class="order-track-text-sub">21st November, 2024</span>
+              </div>
+            </div>
+            <div class="order-track-step step-done">
+              <div class="order-track-status">
+                <span class="order-track-status-dot"></span>
+              </div>
+              <div class="order-track-text">
+                <p class="order-track-text-stat">Manufracturing In Progress</p>
+                <span class="order-track-text-sub">21st November, 2024</span>
+              </div>
+            </div>
+            <div class="order-track-step step-done">
+              <div class="order-track-status">
+                <span class="order-track-status-dot"></span>
+              </div>
+              <div class="order-track-text">
+                <p class="order-track-text-stat">Order Dispatched</p>
+                <span class="order-track-text-sub">21st November, 2024</span>
+              </div>
+            </div>
+            <div class="order-track-step step-done">
+              <div class="order-track-status">
+                <span class="order-track-status-dot"></span>
+              </div>
+              <div class="order-track-text">
+                <p class="order-track-text-stat">Order Deliverd</p>
+                <span class="order-track-text-sub">21st November, 2024</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-12">
+        <h3 class="common-main-title mb-30">Order <span>History</span></h3>
+        <div class="order-track-product-item">
+          <div class="order-track-product-cart">
+            <div class="order-track-product-mdiv">
+              <div class="order-track-product-left">
+                <a href="javascript:void(0)" class="order-track-img-div">
+                  <img src="./assets/images/product1.webp" alt="product 1">
+                </a>
+                <div class="order-track-detail-div">
+                  <a href="javascript:void(0)" class="order-track-name">100% Pure Cotton Jumpsuit</a>
+                  <p class="category">Quantity: <span class="fw-bold">4x</span></p>
+                  <p class="cart-price-p">₹100</p>
+                </div>
+              </div>
+              <div class="order-track-product-right">
+                <a href="javascript:void(0)" class="common-btn common-btn2" title="Repeat Order">Repeat Order</a>
+              </div>
+            </div>
+            <div class="order-track-product-mdiv">
+              <div class="order-track-product-left">
+                <a href="javascript:void(0)" class="order-track-img-div">
+                  <img src="./assets/images/product1.webp" alt="product 1">
+                </a>
+                <div class="order-track-detail-div">
+                  <a href="javascript:void(0)" class="order-track-name">100% Pure Cotton Jumpsuit</a>
+                  <p class="category">Quantity: <span class="fw-bold">4x</span></p>
+                  <p class="cart-price-p">₹100</p>
+                </div>
+              </div>
+              <div class="order-track-product-right">
+                <a href="javascript:void(0)" class="common-btn common-btn2" title="Repeat Order">Repeat Order</a>
+              </div>
+            </div>
+            <div class="order-track-product-mdiv">
+              <div class="order-track-product-left">
+                <a href="javascript:void(0)" class="order-track-img-div">
+                  <img src="./assets/images/product1.webp" alt="product 1">
+                </a>
+                <div class="order-track-detail-div">
+                  <a href="javascript:void(0)" class="order-track-name">100% Pure Cotton Jumpsuit</a>
+                  <p class="category">Quantity: <span class="fw-bold">4x</span></p>
+                  <p class="cart-price-p">₹100</p>
+                </div>
+              </div>
+              <div class="order-track-product-right">
+                <a href="javascript:void(0)" class="common-btn common-btn2" title="Repeat Order">Repeat Order</a>
+              </div>
+            </div>
+            <div class="order-track-product-mdiv">
+              <div class="order-track-product-left">
+                <a href="javascript:void(0)" class="order-track-img-div">
+                  <img src="./assets/images/product1.webp" alt="product 1">
+                </a>
+                <div class="order-track-detail-div">
+                  <a href="javascript:void(0)" class="order-track-name">100% Pure Cotton Jumpsuit</a>
+                  <p class="category">Quantity: <span class="fw-bold">4x</span></p>
+                  <p class="cart-price-p">₹100</p>
+                </div>
+              </div>
+              <div class="order-track-product-right">
+                <a href="javascript:void(0)" class="common-btn common-btn2" title="Repeat Order">Repeat Order</a>
+              </div>
+            </div>
+            <div class="order-track-product-mdiv">
+              <div class="order-track-product-left">
+                <a href="javascript:void(0)" class="order-track-img-div">
+                  <img src="./assets/images/product1.webp" alt="product 1">
+                </a>
+                <div class="order-track-detail-div">
+                  <a href="javascript:void(0)" class="order-track-name">100% Pure Cotton Jumpsuit</a>
+                  <p class="category">Quantity: <span class="fw-bold">4x</span></p>
+                  <p class="cart-price-p">₹100</p>
+                </div>
+              </div>
+              <div class="order-track-product-right">
+                <a href="javascript:void(0)" class="common-btn common-btn2" title="Repeat Order">Repeat Order</a>
+              </div>
+            </div>
+            <div class="order-track-product-mdiv">
+              <div class="order-track-product-left">
+                <a href="javascript:void(0)" class="order-track-img-div">
+                  <img src="./assets/images/product1.webp" alt="product 1">
+                </a>
+                <div class="order-track-detail-div">
+                  <a href="javascript:void(0)" class="order-track-name">100% Pure Cotton Jumpsuit</a>
+                  <p class="category">Quantity: <span class="fw-bold">4x</span></p>
+                  <p class="cart-price-p">₹100</p>
+                </div>
+              </div>
+              <div class="order-track-product-right">
+                <a href="javascript:void(0)" class="common-btn common-btn2" title="Repeat Order">Repeat Order</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<?php include 'include/footer.php' ?>
